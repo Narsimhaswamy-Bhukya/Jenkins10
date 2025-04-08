8ad296c3a2ece61513ffbeb8f724faf5dd865790
Narsimhaswamy-Bhukya <computer.advanced.bns@gmail.com>
2025-04-08 22:15:46 +0530

    again eddited data

--- Helo.java
+++ Helo.java
@@ -1,6 +1,7 @@
 class Helo {
     public static void main(String arg[]){
         System.out.println("Narsimhaswamy");
-        System.out.println("welcome to the lbsnaa OTs for 7202"); 
+        System.out.println("welcome to the lbsnaa OTs for 7202");
+        System.out.println("Bharath Dharsan:winter study tour all india visit");
     }
 }
